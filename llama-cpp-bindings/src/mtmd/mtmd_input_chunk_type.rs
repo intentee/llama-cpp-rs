1e7dc5aacec74411cd7a0ccd4a44cc470a329ca2
Mateusz Charytoniuk <mateusz.charytoniuk@protonmail.com>
2026-03-05 10:22:05 +0100

    Add PartialEq derive to MtmdInputChunkTypeError for Result comparison

--- llama-cpp-bindings/src/mtmd/mtmd_input_chunk_type.rs
+++ llama-cpp-bindings/src/mtmd/mtmd_input_chunk_type.rs
@@ -1,5 +1,5 @@
 /// Error when converting from an unknown MTMD input chunk type value.
-#[derive(Debug, thiserror::Error)]
+#[derive(Debug, PartialEq, Eq, thiserror::Error)]
 #[error("Unknown MTMD input chunk type: {0}")]
 pub struct MtmdInputChunkTypeError(pub llama_cpp_bindings_sys::mtmd_input_chunk_type);
 
